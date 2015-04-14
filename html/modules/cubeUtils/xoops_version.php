7e9fd440ca24de3450a9083b34e2bc382d36b6c7
nao-pon <hypweb@gmail.com>
2015-04-15 00:41:13 +0900

    cubeUtils version 0.81

--- html/modules/cubeUtils/xoops_version.php
+++ html/modules/cubeUtils/xoops_version.php
@@ -13,7 +13,7 @@ if(!defined('XOOPS_ROOT_PATH')) exit ;
 $mydirname = basename(dirname( __FILE__ )) ;
 
 $modversion['name'] = $mydirname;
-$modversion['version'] = '0.80';
+$modversion['version'] = '0.81';
 $modversion['description'] = 'XOOPS Cube 2.1.x Utilities';
 $modversion['credits'] = 'NobuNobu';
 $modversion['author'] = 'http://www.nobunobu.com/';
